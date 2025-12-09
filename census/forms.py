e9cce2725dcfd18a304413355c8eb2097d1d5fe3
Antoine Paris <paris.antoine.paris@gmail.com>
2025-12-09 21:26:10 +0100

    Add app files

---
+++ census/forms.py
@@ -0,0 +1,83 @@
+from django import forms
+
+from django.forms import ModelForm
+
+from .models import Farm, MarketGardener
+
+class EmailForm(forms.Form):
+    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+        # Replace class attribute of form fields with form-control/form-select for proper styling with Bootstrap 5
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = 'Adresse e-mail de la ferme'
+
+    # Check if the given email address matches one in a given list
+    def check_match(self, email_list):
+        if self.cleaned_data['email'] in email_list:
+            return True
+        else:
+            return False
+
+class FarmForm(ModelForm):
+    class Meta:
+        model = Farm
+        exclude = ['comment', 'last_update', 'flagged', 'edited_by_user', 'added_by', 'public']
+
+        # labels (verbose_name in the model) and help_texts can be overridden too, but better change it directly in the model
+        error_messages ={
+            'email' : {
+                'unique': "Une ferme maraîchère avec cette adresse e-mail existe déjà."
+            },
+            'phone' : {
+                'invalid': "Veuillez indiquer un numéro de téléphone valide (GSM ou fixe).",
+                'unique': "Une ferme maraîchère avec ce numéro de téléphone existe déjà."
+            },
+            'website' : {
+                'unique': "Une ferme maraîchère avec ce site web existe déjà."
+            },
+            'fb_page' : {
+                'unique': "Une ferme maraîchère avec cette page Facebook existe déjà."
+            }
+
+        }
+
+    # Replace class attribute of form fields with form-control/form-select for proper styling with Bootstrap 5
+    def __init__(self, *args, **kwargs):
+        super(FarmForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            if visible.widget_type == 'select':
+                visible.field.widget.attrs['class'] = 'form-select'
+            elif visible.widget_type == 'checkbox':
+                visible.field.widget.attrs['class'] = 'form-check-input'
+            else:
+                visible.field.widget.attrs['class'] = 'form-control'
+
+            if visible.name == 'GPS_coordinates':
+                visible.field.widget.attrs['placeholder'] = '50.66677974362429, 4.620421360598303'
+
+class MarketGardenerForm(ModelForm):
+    class Meta:
+        model = MarketGardener
+        exclude = ['farm']
+
+        error_messages ={
+            'email' : {
+                'unique': "Un·e maraîcher·ère avec cette adresse e-mail existe déjà."
+            },
+            'phone' : {
+                'invalid': "Veuillez indiquer un numéro de téléphone valide (GSM ou fixe).",
+                'unique': "Un·e maraîcher·ère avec ce numéro de téléphone existe déjà."
+            }
+        }
+
+    # Replace class attribute of form fields with form-control/form-select for proper styling with Bootstrap 5
+    def __init__(self, *args, **kwargs):
+        super(MarketGardenerForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            if visible.widget_type == 'checkbox':
+                visible.field.widget.attrs['class'] = 'form-check-input'
+            else:
+                visible.field.widget.attrs['class'] = 'form-control'
---
+++ census/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'census'
+
+# the name parameter is used in {% url %} in the templates
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("listing/", views.ListingView.as_view(), name="listing"),
+    path("map/", views.MapView.as_view(), name="map"),
+    path("view/<int:pk>/", views.FarmView.as_view(), name="view"),
+    path("create/", views.FarmCreateView.as_view(), name="create"),
+    path("thanks/<int:farm_id>/", views.thanks, name="thanks"),
+    path("update/<str:token>/", views.FarmUpdateView.as_view(), name="update"),
+]
