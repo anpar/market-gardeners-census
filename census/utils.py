2cdd420fd979c3daa7a6a01d381853f5bfd47d09
Antoine Paris <paris.antoine.paris@gmail.com>
2026-03-09 09:41:03 +0100

    Handle empty list in send_email() + reword campaign email for new farms

--- census/utils.py
+++ census/utils.py
@@ -2,27 +2,29 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 def send_email(to, subject, template, context):
-    text_content = render_to_string(
-        "census/mails/" + template + ".txt",
-        context=context,
-    )
+    # Avoid a crash if I select a farm with no known emails when sending a campaign
+    if len(to) > 0:
+        text_content = render_to_string(
+            "census/mails/" + template + ".txt",
+            context=context,
+        )
 
-    html_content = render_to_string(
-        "census/mails/" + template + ".html",
-        context=context,
-    )
+        html_content = render_to_string(
+            "census/mails/" + template + ".html",
+            context=context,
+        )
 
-    cc = ["antoine.paris@uclouvain.be"]
-    if to[0] == cc[0]:
-        cc = None
+        cc = ["antoine.paris@uclouvain.be"]
+        if to[0] == cc[0]:
+            cc = None
 
-    email = EmailMultiAlternatives(
-        subject=subject,
-        body=text_content,
-        from_email="Antoine Paris <recensement@maraichage-wallonie.be>",
-        to=to,
-        cc=cc,
-    )
+        email = EmailMultiAlternatives(
+            subject=subject,
+            body=text_content,
+            from_email="Antoine Paris <recensement@maraichage-wallonie.be>",
+            to=to,
+            cc=cc,
+        )
 
-    email.attach_alternative(html_content, "text/html")
-    email.send()
+        email.attach_alternative(html_content, "text/html")
+        email.send()
